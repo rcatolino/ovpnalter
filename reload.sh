1f56d45e14f1705578962a01ad9716355bc397d5
Raphael Catolino <raphael.catolino@gmail.com>
2020-10-08 07:37:33 +0100

    Also load ebpf program on ingress

--- reload.sh
+++ reload.sh
@@ -3,3 +3,4 @@ clang -fno-stack-protector -O2 -target bpf -c filteralter.c -o filteralter.o
 sudo tc qdisc delete dev enp2s0.6 clsact
 sudo tc qdisc add dev enp2s0.6 clsact
 sudo tc filter add dev enp2s0.6 egress bpf direct-action obj filteralter.o sec .text
+sudo tc filter add dev enp2s0.6 ingress bpf direct-action obj filteralter.o sec .text
